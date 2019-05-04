8577873e92e7b7cd9b9b3c11612479e8ba3575b0
Artem Sheremet <dot.doom@gmail.com>
2019-05-04 18:58:42 +0200

    Create a new XCode project

---
+++ cocoapodsdemo/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  cocoapodsdemo
+//
+//  Copyright © 2019 com.example. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cocoapodsdemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  cocoapodsdemo
+//
+//  Copyright © 2019 com.example. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
